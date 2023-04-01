23b6151d71dce8ba56240303acef4fcf291a91dc
Yukyung Kim / Sally <rladbrud12@naver.com>
2023-04-01 20:34:22 +0900

    fix : css variable

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,25 +3,4 @@ import components from "./components";
 
 export const theme = extendTheme({
   components,
-  colors: {
-    primary: {
-      100: "#9be6b9",
-      200: "#8be2ae",
-      300: "#7adda2",
-      400: "#6ad997",
-      500: "#59d58b", // main
-      600: "#50c07d",
-      700: "#47aa6f",
-      800: "#3e9561",
-      900: "#358053",
-    },
-    white: "#ffffff",
-    black: "#191c19",
-  },
-  header: {
-    maxWidth: "1024px",
-  },
-  padding: {
-    mobileGap: "var(--mobile-padding)",
-  },
 });
